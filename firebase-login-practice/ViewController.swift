ec9e0d32909db18c08ce606488113e9716f27b2f
adrpadua <adrpadua@gmail.com>
2016-07-10 19:43:30 -0700

    Firebase added with Cocoapods. AppDelegate changed for Firebase Info.plist also changed for Firebase Logins work. Facebook login button works.

--- firebase-login-practice/ViewController.swift
+++ firebase-login-practice/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func fbBtnPressed(sender: UIButton) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], handler: {
+            (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookLogin)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with facebook. \(accessToken)")
+            }
+        })
+    }
 }
 
