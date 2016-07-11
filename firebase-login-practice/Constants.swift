ebdf656e1b75b122142850cd947449aa23fb78ad
adrpadua <adrpadua@gmail.com>
2016-07-10 18:58:22 -0700

    Login page added Custom views used for Buttons, Views, and TextFields.

---
+++ firebase-login-practice/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  firebase-login-practice
+//
+//  Created by Adrian Padua on 7/10/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
