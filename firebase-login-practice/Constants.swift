fda5134403e8952344059e82e24efc6d44bbc6ca
adrpadua <adrpadua@gmail.com>
2016-07-10 20:59:19 -0700

    Email Logins added.

--- firebase-login-practice/Constants.swift
+++ firebase-login-practice/Constants.swift
@@ -10,3 +10,12 @@ import Foundation
 import UIKit
 
 let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
+// KEYS
+let KEY_UID = "uid"
+
+// SEGUES
+let SEGUE_LOGGED_IN = "loggedIn"
+
+// STATUS CODES
+let STATUS_ACCOUNT_NONEXIST = 17011
--- firebase-login-practice/ViewController.swift
+++ firebase-login-practice/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) != nil {
+            self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+        }
     }
 
     @IBAction func fbBtnPressed(sender: UIButton) {
@@ -33,8 +45,77 @@ class ViewController: UIViewController {
             } else {
                 let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
                 print("Successfully logged in with facebook. \(accessToken)")
+                
+                //DataService.ds.REF_BASE.authWithOAuthProvider(")
+                
+                let credential = FIRFacebookAuthProvider.credentialWithAccessToken(accessToken)
+                
+                FIRAuth.auth()?.signInWithCredential(credential, completion: { (user, error) in
+                    
+                    if error != nil {
+                        print("Login failed. \(error)")
+                    } else {
+                        print("Logged in. \(user)")
+                        
+                        let userData = ["provider": credential.provider]
+                        DataService.ds.createFirebaseUser((user?.uid)!, user: userData)
+                        
+                        NSUserDefaults.standardUserDefaults().setValue(user!.uid, forKey: KEY_UID)
+                        self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                    }
+                    
+                })
+                
             }
         })
     }
+    
+    @IBAction func attemptLogin(sender: UIButton!) {
+        
+        if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+            
+            FIRAuth.auth()?.signInWithEmail(email, password: pwd, completion: { (user, error) in
+                
+                if error != nil {
+                    print(error!)
+                    
+                    if error!.code == STATUS_ACCOUNT_NONEXIST {
+                        FIRAuth.auth()?.createUserWithEmail(email, password: pwd, completion: { (user, error) in
+                            
+                            if error != nil {
+                                self.showErrorAlert("Could not create account", msg: "Problem creating account. Try something else")
+                            } else {
+                                NSUserDefaults.standardUserDefaults().setValue(user!.uid, forKey: KEY_UID)
+                                
+                                let userData = ["provider" : "email"]
+                                DataService.ds.createFirebaseUser((user?.uid)!, user: userData)
+                                
+                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                            }
+                        
+                        })
+                    } else {
+                        self.showErrorAlert("Could Not Log In", msg: "Please check your username and password.")
+                    }
+                    
+                } else {
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
+                
+            })
+            
+        } else {
+            showErrorAlert("Email and Password Required", msg: "You must enter an email and a password")
+        }
+    }
+    
+    func showErrorAlert(title: String, msg: String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
+
+
 
