7a8cb09f3dd987be7751038f485b5905aa18de83
adrpadua <adrpadua@gmail.com>
2016-07-11 07:55:31 -0700

    Created post-feed ViewController. Added more assets to serve as test data Created custom View classes for Posts

---
+++ firebase-login-practice/FeedVC.swift
@@ -0,0 +1,34 @@
+//
+//  FeedVC.swift
+//  firebase-login-practice
+//
+//  Created by Adrian Padua on 7/11/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+}
---
+++ firebase-login-practice/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  firebase-login-practice
+//
+//  Created by Adrian Padua on 7/11/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    // Happens after view has a size
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
